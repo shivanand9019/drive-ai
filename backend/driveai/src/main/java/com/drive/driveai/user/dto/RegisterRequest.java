d5c876ac296535d516b86831d21cee65afd3eb58
shivanand9019 <shivanandmadar512@gmail.com>
2026-07-18 00:46:59 +0530

    feat(user): add registration DTOs

---
+++ backend/driveai/src/main/java/com/drive/driveai/user/dto/RegisterRequest.java
@@ -0,0 +1,15 @@
+package com.drive.driveai.user.dto;
+
+
+import lombok.Data;
+
+@Data
+public class RegisterRequest {
+    private String fullName;
+    private String email;
+    private String password;
+   
+
+
+    
+}
