b1e2e4ccb72e4c03609ab6bbab95efd6757e44b2
shivanand9019 <shivanandmadar512@gmail.com>
2026-07-18 22:23:16 +0530

    feat: implement user registration with Spring Security configuration

--- backend/driveai/src/main/java/com/drive/driveai/user/repository/UserRepository.java
+++ backend/driveai/src/main/java/com/drive/driveai/user/repository/UserRepository.java
@@ -5,6 +5,9 @@ import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.drive.driveai.user.entity.User;
 public interface UserRepository extends JpaRepository<User, UUID> {
+
     Optional<User> findByEmail(String email);
+   
+    boolean existsByEmail(String email);
 
 }
---
+++ backend/driveai/src/main/java/com/drive/driveai/user/service/UserService.java
@@ -0,0 +1,68 @@
+package com.drive.driveai.user.service;
+
+import java.time.LocalDateTime;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.drive.driveai.user.dto.RegisterRequest;
+import com.drive.driveai.user.dto.RegisterResponse;
+import com.drive.driveai.user.entity.User;
+import com.drive.driveai.user.enums.AccountStatus;
+import com.drive.driveai.user.enums.Role;
+import com.drive.driveai.user.mapper.UserMapper;
+import com.drive.driveai.user.repository.UserRepository;
+
+@Service
+public class UserService {
+  
+    private final UserMapper userMapper;
+    
+    private final UserRepository userRepository;
+ 
+    private final PasswordEncoder passwordEncoder;
+    
+    public UserService(UserMapper userMapper,UserRepository userRepository,PasswordEncoder passwordEncoder){
+        this.userMapper = userMapper;
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+
+    }
+    
+    
+
+    
+
+    public RegisterResponse registerUser(RegisterRequest request) {
+
+        // check exists
+        String email = request.getEmail().trim().toLowerCase();
+        if(userRepository.existsByEmail(email)){
+           throw new RuntimeException("User Already Exists");
+        }
+        User user = userMapper.mapToEntity(request);
+        
+        String hashedPassword = passwordEncoder.encode(request.getPassword());
+
+        user.setPasswordHash(hashedPassword);
+    
+        user.setStatus(AccountStatus.ACTIVE);
+        user.setEmailVerified(false);
+        user.setRole(Role.USER);
+
+        user.setCreatedAt(LocalDateTime.now());
+
+        user.setUpdatedAt(null);
+        user.setLastLoginAt(null);
+        user.setDeletedAt(null);
+        User savedUser = userRepository.save(user);
+
+
+        RegisterResponse response = userMapper.mapToResponse(savedUser);
+        
+
+        return response;
+       
+    }
+    
+}
